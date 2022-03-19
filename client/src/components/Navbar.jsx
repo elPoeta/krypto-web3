4086cf7d065945cc547781de756e5f601c88ff25
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2022-03-19 20:06:03 -0300

    style nav menu mobile

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,23 +1,42 @@
+import { useState } from 'react';
+import { MenuAlt4Icon, XIcon } from '@heroicons/react/outline';
 import logo from '../images/logo.png'
 
+const menuItems = ["Market", "Exchange", "Wallets"];
 const NavBarItem = ({ title, classProps }) => (
   <li className={`mx-4 cursor-pointer ${classProps}`}>{title}</li>
 );
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
     <nav className='w-full flex md:justify-center justify-between items-center p-4'>
       <div className='md:flex-[0.5] flex-initial justify-center items-center'>
         <img src={logo} alt="logo" className='w-32 cursor-pointer' />
       </div>
       <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
-        {["Market", "Exchange", "Wallets"].map((item, index) => (
+        {menuItems.map((item, index) => (
           <NavBarItem key={item + index} title={item} />
         ))}
         <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
           Login
         </li>
       </ul>
+      <div className="flex relative">
+        {!toggleMenu && (
+          <MenuAlt4Icon className="h-5 w-5 text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(true)} />
+        )}
+        {toggleMenu && (
+          <ul
+            className="z-10 fixed top-0 right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in"
+          >
+            <li className="text-xl w-full my-2"><XIcon className="h-5 w-5 text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(false)} /></li>
+            {menuItems.map(
+              (item, index) => <NavBarItem key={item + index} title={item} classprops="my-2 text-lg" />,
+            )}
+          </ul>
+        )}
+      </div>
     </nav>
   )
 }
